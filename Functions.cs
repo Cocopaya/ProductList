5f7ce8affedf8e10707faabd0bfcc48972f02017
Cocopaya <233475834+Cocopaya@users.noreply.github.com>
2026-09-03 14:34:16 +0200

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_List_Level_1
+{
+    internal class Functions
+    {
+        public static List<string> AddProducts(List<string> currentList)
+        {
+            Console.WriteLine("Enter product names");
+            Console.WriteLine("Type exit to finish");
+            Console.WriteLine("-------------------");
+
+            List<string> newProducts = [];
+
+            while (newProducts.Count < 10)
+            {
+                string userInput = Console.ReadLine();
+                string response = AddProduct(userInput);
+
+                if (response == "exit")
+                {
+                    break;
+                }
+                else if (response == userInput)
+                {
+
+                    if (newProducts.Contains(userInput) || currentList.Contains(userInput))
+                    {
+                        Console.WriteLine("WARNING: Product already exists");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product Added: {response}");
+                        newProducts.Add(userInput);
+                    }
+
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(response);
+                    continue;
+                }
+            }
+            return newProducts;
+        }
+        public static string AddProduct(string userInput)
+        {
+                string[] inputParts = userInput.Split('-');
+
+                if (userInput.ToLower().Trim() == "exit")
+                {
+                    return "exit";
+                }
+                if (userInput.Trim().Length == 0)
+                {
+                    return "Input can not be empty";
+                }
+                if (!userInput.Contains("-"))
+                {
+                    return "Product must contain a dash (-)";
+                }
+                if (!inputParts[0].All(char.IsLetter))
+                {
+                    return "The left side must contain letters only";
+                }
+                if (!inputParts[1].All(char.IsDigit))
+                {
+                    return "The right side must contain numbers only";
+                }
+                else if (Convert.ToInt32(inputParts[1]) < 200 || (Convert.ToInt32(inputParts[1]) > 500))
+                {
+                    return "The numeric part must be between 200 and 500";
+                }
+                else 
+                {
+                    return userInput;
+                }
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using Product_List_Level_1;
+
+List<string> products = [];
+bool inventoryOpen = true;
+
+Console.WriteLine("PRODUCT INVENTORY SYSTEM");
+
+while (inventoryOpen)
+{
+    Console.WriteLine("1. Add Products");
+    Console.WriteLine("2. View Products");
+    Console.WriteLine("3. Search Product");
+    Console.WriteLine("4. Delete Product");
+    Console.WriteLine("5. Statistics");
+    Console.WriteLine("6. Exit");
+
+    string choice = Console.ReadLine();
+
+    // Add Products
+    if (choice == "1")
+    {
+        List<string> newProducts = Functions.AddProducts(products);
+
+        foreach (string product in newProducts)
+        {
+            products.Add(product);
+        }
+
+        continue;
+    }
+
+    // View Products
+    else if (choice == "2")
+    {
+        List<string> copy = new List<string>(products);
+        copy.Sort();
+        foreach (string product in copy)
+        {
+            Console.WriteLine(product);
+        }
+
+        continue;
+    }
+
+    // Search Product
+    else if (choice == "3")
+    {
+        string userInput = Console.ReadLine();
+        foreach (string product in products)
+        {
+            if (product.Contains(userInput))
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        continue;
+    }
+
+    // Delete Product
+    else if (choice == "4")
+    {
+        string userInput = Console.ReadLine();
+        foreach (string product in products)
+        {
+            if (product == userInput)
+            {
+                products.Remove(product);
+                Console.WriteLine($"Removed product: {userInput}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't find product: {userInput}");
+            }
+        }
+
+        continue;
+    }
+
+    else if (choice == "5")
+    {
+        Console.WriteLine("Statistics");
+        Console.WriteLine($"Total Products: {products.Count}");
+        int lowestNumber = 500;
+        int highestNumber = 0;
+        int sum = 0;
+
+        foreach (string product in products)
+        {
+            string[] parts = product.Split('-');
+            int productNumber = Convert.ToInt32(parts[1]);
+            if (productNumber < lowestNumber)
+            {
+                lowestNumber = productNumber;
+            }
+            if (productNumber > highestNumber)
+            {
+                highestNumber = productNumber;
+            }
+            sum += productNumber;
+        }
+
+        Console.WriteLine($"Lowest Number: {lowestNumber}");
+        Console.WriteLine($"Highest Number: {highestNumber}");
+        Console.WriteLine($"Average Number: {sum / products.Count}");
+    }
+
+    // Exit Application
+    else if (choice == "6")
+    {
+        inventoryOpen = false;
+    }
+}
+
+Console.ReadLine();
